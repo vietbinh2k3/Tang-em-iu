baafd55af3689c3dee6a842e52a7f9d922f9fd89
ducanh1a2b3c <69542325+ducanh1a2b3c@users.noreply.github.com>
2021-06-04 04:24:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 8r anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100008031420972/' 
+    messLink: 'facebook.com/messages/t/100035566904288' 
 }
